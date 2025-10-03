237e128276cb9ca91d7acfdb4f4d6c12dd7ef506
Indrajeet <indrajeetdhaked720@gmail.com>
2025-10-03 22:37:35 +0530

    all update

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="top-strip py-2 text-primary">
+            <div className="container">
+                <div className="flex items-center justify-between">
+                    <div className="col1 w-[50%]">
+                        <p className='text-[14px]'>
+                            Get up to 50% off mew season styels, limited time
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
